eb337f901d0f5cbb190a621db831704b25374673
toyhammered <daniel@rassiner.com>
2021-10-17 17:38:06 -0700

    more tests

--- spec/graphql/fancy_loader_spec.rb
+++ spec/graphql/fancy_loader_spec.rb
@@ -1,9 +1,5 @@
 RSpec.describe GraphQL::FancyLoader do
   it 'has a version number' do
-    expect(GraphQL::FancyLoader::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
+    expect(GraphQL::FancyLoader::VERSION).not_to be_nil
   end
 end
